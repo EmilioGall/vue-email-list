291114a60a1a8d81ae23f550230d656c1ebce94f
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-29 21:48:30 +0200

    add: fill array with new email 10 times - html&js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ createApp({
 
          newEmail: "",
 
+
+
          arrEmailList: [],
 
       };
@@ -26,19 +28,57 @@ createApp({
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((resp) => {
 
-               console.log("response", resp.data.response);
+               // console.log("response", resp.data.response);
 
                this.newEmail = resp.data.response;
 
-               console.log("newEmail", this.newEmail);
+               console.log("newEmail", this.newEmail, typeof this.newEmail);
+
+               if (this.newEmail !== "") {
+
+                  this.pushInArrEmailList();
+   
+                  console.log("--------------------");
+   
+                  console.log("arrEmailList", this.arrEmailList);
+   
+                  this.newEmail = "";
+   
+               } else {
+   
+                  this.generateEmail();
+   
+               };
 
             });
+
+      },
+
+      pushInArrEmailList: function () {
+
+         this.arrEmailList.push(this.newEmail);
+
+      },
+
+      fillArrEmailList: function () {
+
+         let i = 0;
+
+         while (i < 10) {
+
+            this.generateEmail();
+
+            i++;
+
+         };
 
       },
 
       cleanArrEmailList: function () {
 
          arrEmailList = [];
+
+         // console.log("arrEmailList", this.arrEmailList);
 
       },
 
