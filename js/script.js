c4b5ea7c51d544c97db70469b929ff0c03d1dfa4
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-29 20:21:46 +0200

    add: email generation - js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ createApp({
    data() {
       return {
 
-         //
+         newEmail: "",
+
+         arrEmailList: [],
 
       };
    },
@@ -18,7 +20,27 @@ createApp({
 
    methods: {
 
-      //
+      generateEmail: function () {
+
+         axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+
+               console.log("response", resp.data.response);
+
+               this.newEmail = resp.data.response;
+
+               console.log("newEmail", this.newEmail);
+
+            });
+
+      },
+
+      cleanArrEmailList: function () {
+
+         arrEmailList = [];
+
+      },
 
    },
 
